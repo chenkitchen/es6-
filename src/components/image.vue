4948a20a0ef5f708eea3693552529e2ce2d65319
chenkitchen <1591584907@qq.com>
2021-12-19 15:46:21 +0800

    requestAnimation

--- src/components/image.vue
+++ src/components/image.vue
@@ -1,8 +1,10 @@
 <template>
     <div id="box">
         <p style="text-align:center">
-            <img src="./cat.png" alt="cat"  style="position:relative">
+            <img src="./cat.png" alt="cat"  style="position:relative" id="cat">
         </p>
+        <p>{{showTime}}</p>
+        <button @click="end">stop</button>
     </div>
 </template>
 
@@ -11,25 +13,49 @@
         data(){
             return {
                 cat:null,
-                angle:null,
+                angle:Math.PI / 2,
+                animationFrameId:null,
+                showTime:null,
             }
         },
         mounted(){
              
-             this.$nextTick(()=>{
-                 this.cat = document.querySelector('img');
-             })
-            this.angle = Math.PI / 2;
-            requestAnimationFrame(this.animate);
+            //  this.$nextTick(()=>{
+            //      this.cat = document.querySelector('img');
+            //  })
+            // window.requestAnimationFrame(this.animate);
+            this.start();
+        },
+        destroyed(){
+
         },
         methods:{
             animate(time,lastTime){
+                let cat = document.querySelector('img');
                 if(lastTime != null){
                     this.angle += (time - lastTime) * 0.001;
                 }
-                this.cat.style.top = (Math.sin(this.angle)*20) + 'px';
-                this.cat.style.left = (Math.cos(this.angle)*200) + 'px';
-                requestAnimationFrame(newTime => this.animate(newTime,time))
+                cat.style.top = (Math.sin(this.angle)*20) + 'px';
+                cat.style.left = (Math.cos(this.angle)*200) + 'px';
+                window.requestAnimationFrame(newTime => this.animate(newTime,time))
+            },
+            async start(){
+                const f  = async ()=>{
+                    // console.log('测试', new Date());
+                    this.showTime = new Date().toLocaleString();
+                     let cat = await document.getElementById("cat");
+                     this.angle += 0.011;
+                    cat.style.top = (Math.sin(this.angle)*20) + 'px';
+                    cat.style.left = (Math.cos(this.angle)*200) + 'px';
+                    this.animationFrameId = requestAnimationFrame(f)
+                }
+                if(!this.animationFrameId){
+                    this.animationFrameId = requestAnimationFrame(f);
+                }
+                return true;
+            },
+            end(){
+                cancelAnimationFrame(this.animationFrameId);
             }
         }
     }
